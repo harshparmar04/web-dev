e68230a27195c74b9971b4a542f97bce1c9d0ee4
harshparmar <parmarharsh4296@gmail.com>
2022-03-21 20:22:55 -0400

    Changes done

--- src/components/tuiter/NavigationSidebar/index.js
+++ src/components/tuiter/NavigationSidebar/index.js
@@ -21,42 +21,42 @@ const NavigationSidebar = ({active = 'explore'}) => {
                     <i className=" fa fa-hashtag pe-1"></i>
                     <span className=" d-none d-xl-inline-block ">Explore</span>
                 </Link>
-                <a className={`list-group-item list-group-item-action ${
+                <Link className={`list-group-item list-group-item-action ${
                     active === " notifications" ? " active" : ""
-                }`} href="google.com" alt="">
+                }`} to="/tuiter/notifications">
                     <i className="fa fa-bell pe-1"></i>
                     <span className="d-none d-xl-inline-block ">Notifications</span>
-                </a>
-                <a className={`list-group-item list-group-item-action ${
+                </Link>
+                <Link className={`list-group-item list-group-item-action ${
                     active === " messages" ? " active" : ""
-                }`} href="google.com" alt="">
+                }`} to="/tuiter/messages">
                     <i className="fa fa-envelope pe-1"></i>
                     <span className="d-none d-xl-inline-block ">Messages</span>
-                </a>
-                <a className={`list-group-item list-group-item-action ${
+                </Link>
+                <Link className={`list-group-item list-group-item-action ${
                     active === " bookmarks" ? " active" : ""
-                }`} href="google.com" alt="">
+                }`} to="/tuiter/bookmarks">
                     <i className="fa fa-bookmark pe-1"></i>
                     <span className="d-none d-xl-inline-block ">Bookmarks</span>
-                </a>
-                <a className={`list-group-item list-group-item-action ${
+                </Link>
+                <Link className={`list-group-item list-group-item-action ${
                     active === " lists" ? " active" : ""
-                }`} href="google.com" alt="">
+                }`} to="/tuiter/lists">
                     <i className="fas fa-list pe-1"></i>
                     <span className="d-none d-xl-inline-block ">Lists</span>
-                </a>
-                <a className={`list-group-item list-group-item-action ${
+                </Link>
+                <Link className={`list-group-item list-group-item-action ${
                     active === " profile" ? " active" : ""
-                }`} href="google.com" alt="">
+                }`} to="/tuiter/profile">
                     <i className="fa fa-user pe-1"></i>
                     <span className="d-none d-xl-inline-block ">Profile</span>
-                </a>
-                <a className={`list-group-item list-group-item-action ${
+                </Link>
+                <Link className={`list-group-item list-group-item-action ${
                     active === " more" ? " active" : ""
-                }`} href="google.com" alt="">
+                }`} to="/tuiter/more">
                     <i className="fas fa-minus-circle pe-1"></i>
                     <span className="d-none d-xl-inline-block ">More</span>
-                </a>
+                </Link>
             </div>
             <div className="d-grid mt-2">
                 <a href="tweet.html"
