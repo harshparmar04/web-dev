890ce364656ac641e555dc386a446484ea8d0e30
harshparmar <parmarharsh4296@gmail.com>
2022-03-28 22:09:55 -0400

    a7 Redux sp22

--- src/components/tuiter/reducers/profile-reducer.js
+++ src/components/tuiter/reducers/profile-reducer.js
@@ -1,9 +1,7 @@
 import profile from "../data/profile.json"
 
 const ProfileReducer = (state = profile, action) => {
-    console.log("before save-edit");
     if (action.type === 'save-edit') {
-        console.log("save-edit" + JSON.stringify(action.data));
         return action.data;
     }
 
